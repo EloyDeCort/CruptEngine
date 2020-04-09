3529b582d1b9a43988f70450d06fb376e4358c65
EloyDeCort <eloy.decort@gmail.com>
2020-04-09 18:45:20 +0200

    [MOD] Fixed all dependencies and linker/build errors. [MOD] Game now correctly linked to Engine

--- CruptGame/Main.cpp
+++ CruptGame/Main.cpp
@@ -1,6 +1,12 @@
+#include "CruptEnginePCH.h"
+
+#include "SDL.h"
+
+
 #include "CruptEngine.h"
 
-int main()
-{
-	return 0;
+int main(int, char*[]) {
+	crupt::CruptEngine engine;
+	engine.Run();
+    return 0;
 }
